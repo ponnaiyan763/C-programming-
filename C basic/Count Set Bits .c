82222e700c6a27c442c88af5d56611a3bfe63f0e
ponnaiyan <ponnaiyan763@gmail.com>
2026-08-14 19:21:25 +0530

    Add files via upload

---
+++ C basic/Count Set Bits .c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+    int n,c=0;
+    scanf("%d",&n);
+    while (n>0) {
+        if (n&1) {
+            c++;
+        }
+        n=n>>1;
+    }
+    printf("%d",c);
+    return 0;
+}
