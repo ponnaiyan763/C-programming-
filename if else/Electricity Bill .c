06f1243a38bfedde572e6929034a1b1effb80957
ponnaiyan <ponnaiyan763@gmail.com>
2026-08-22 20:53:43 +0530

    Add files via upload

---
+++ if else/Electricity Bill .c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main() {
+    int units;
+    float bill;
+    scanf("%d",&units);
+    if(units<=50) {
+        bill=units*3;
+    }
+    else if(units<=100) {
+        bill=(50*3) + (units-50)*5;
+    }
+    else {
+        bill=(50*3) + (50*5)+(units-100)*7;
+    }
+    printf("Bill Amount =Rs %f",bill);
+    return 0;
+}
