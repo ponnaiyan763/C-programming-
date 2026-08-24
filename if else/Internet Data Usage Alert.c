e9d1a41bb06091b2a49eec36a2c899b9fbd1160a
ponnaiyan <ponnaiyan763@gmail.com>
2026-08-24 17:57:03 +0530

    Add files via upload

---
+++ if else/Internet Data Usage Alert.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+    float a;
+    scanf("%f",&a);
+   if (a>2){
+       printf("Data Limit Exceeded");
+   }
+   else {
+       printf("Data Within Limit");
+   }
+    return 0;
+}
