d1469e2a2866ab767a31a34adf5357e19368e430
ponnaiyan <ponnaiyan763@gmail.com>
2026-08-24 17:58:12 +0530

    Add files via upload

---
+++ if else/Online Order Discount.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+    int a;
+    scanf("%d",&a);
+   if (a>=1000){
+       printf("Discount Applied");
+   }
+   else {
+       printf("No Discount");
+   }
+    return 0;
+}
