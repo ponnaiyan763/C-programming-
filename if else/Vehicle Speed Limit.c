bafe1f1d1504779aea093ce5a357006d2437bf49
ponnaiyan <ponnaiyan763@gmail.com>
2026-08-25 20:40:51 +0530

    Add files via upload

---
+++ if else/Vehicle Speed Limit.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+    int a;
+    scanf("%d",&a);
+   if (a<90){
+       printf("within Speed Limit");
+   }
+   else {
+       printf("Speed Limit Exceeded");
+   }
+    return 0;
+}
