32e30a59dab73ee41fd28b8d3386682ab7f08efe
ponnaiyan <ponnaiyan763@gmail.com>
2026-08-27 10:07:41 +0530

    Add files via upload

---
+++ if else/Login Attempt Counter.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+    int a;
+    scanf("%d",&a);
+   if (a<=3){
+       printf("Account Allowed");
+   }
+   else {
+       printf("Account Allowed");
+   }
+    return 0;
+}
