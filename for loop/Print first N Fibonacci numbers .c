7de4060bacde4223633ab9fbdee0ce9935617c14
ponnaiyan <ponnaiyan763@gmail.com>
2026-08-29 17:46:18 +0530

    Add files via upload

---
+++ for loop/Print first N Fibonacci numbers .c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main() {
+    int n,temp=0;
+    scanf("%d",&n);
+    int first=0;
+    int second=1;
+
+
+    if (1<=n) {
+        printf("%d ",first);
+    }
+    if (2<=n) {
+        printf("%d ",second);
+    }
+
+    for (int i=2;i<n;i++) {
+        temp=first+second;
+        first=second;
+        second=temp;
+        printf("%d ",temp);
+    }
+    return 0;
+}
